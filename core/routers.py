4efd37ed95e4e47476a9698a954126be57ca6a70
Kolawole <onaelmangabo@gmail.com>
2021-05-13 00:36:27 +0200

    - fix: fix typo

--- core/routers.py
+++ core/routers.py
@@ -5,10 +5,13 @@ from core.auth.viewsets import LoginViewSet, RegistrationViewSet, RefreshViewSet
 
 routes = SimpleRouter()
 
-routes.register(r'user', UserViewSet, basename='user')
+# AUTHENTICATION
 routes.register(r'auth/login', LoginViewSet, basename='auth-login')
 routes.register(r'auth/register', RegistrationViewSet, basename='auth-register')
 routes.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+
+# USER
+routes.register(r'user', UserViewSet, basename='user')
 
 
 urlpatterns = [
