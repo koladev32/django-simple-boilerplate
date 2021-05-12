878fc0a10a5b091aed44fa80c7e940c47e9006ad
Kolawole <onaelmangabo@gmail.com>
2021-05-12 14:21:44 +0200

    - fix typo

--- core/auth/serializers/register.py
+++ core/auth/serializers/register.py
@@ -8,8 +8,6 @@ from core.user.models import User
 class RegisterSerializer(UserSerializer):
     password = serializers.CharField(max_length=128, min_length=8, write_only=True, required=True)
     email = serializers.EmailField(required=True, write_only=True, max_length=128)
-    access = serializers.CharField()
-    refresh = serializers.CharField()
 
     class Meta:
         model = User
